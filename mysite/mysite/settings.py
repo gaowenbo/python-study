0dde76e76a7e42e32bc33548c4cb333036413dbe
gaowenbo <gaowenbo@mankebao.com>
2020-05-27 17:23:27 +0800

    s

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ph231af%lucr+$#zj#q#!6dr%$y_t60%n+vwsooh9w%4mfuy6)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
