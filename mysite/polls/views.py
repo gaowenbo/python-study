eb1a865df23bcdaa890eebda158f8bd6915de267
gaowenbo <gaowenbo@mankebao.com>
2020-05-27 17:44:24 +0800

    s

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,7 @@ import os
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>It is nows %s.</body></html>" % now
     return HttpResponse(html)
 
 
