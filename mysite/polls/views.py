3f40e821579abcb71eae615065590bdf074a19a6
gaowenbo <gaowenbo@mankebao.com>
2020-06-04 18:28:03 +0800

    s

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import pymysql
-import pandas as pd
+
 # Create your views here.
 
 from django.http import HttpResponse, FileResponse
@@ -18,22 +18,22 @@ def gitPull(request):
     return HttpResponse(html)
 
 def wxArr(request):
-    db = pymysql.connect(host="47.95.49.67",
-                         port=8066,
-                         user="sidb",
-                         passwd="vFQ8nsab3Ia",
-                         db="pay")
-
-    cur = db.cursor()
-    cur.execute("select * from zys_arrearage_order where arre_status = 1 and pay_type = 2 limit 99999999")
-    res = cur.fetchall()
-    print(list(cur.description))
-    fields = list(map(lambda x:list(x)[0], list(cur.description)))
-    print(fields)
-    df = pd.DataFrame(res, columns=fields)
-    df.to_excel("wxArr.xls")
-    file = open('wxArr.xls', 'rb')
-    response = FileResponse(file)
-    response['Content-Type'] = 'application/octet-stream'
-    response['Content-Disposition'] = 'attachment;filename="wxArr.xls"'
-    return response
+    # db = pymysql.connect(host="47.95.49.67",
+    #                      port=8066,
+    #                      user="sidb",
+    #                      passwd="vFQ8nsab3Ia",
+    #                      db="pay")
+    #
+    # cur = db.cursor()
+    # cur.execute("select * from zys_arrearage_order where arre_status = 1 and pay_type = 2 limit 99999999")
+    # res = cur.fetchall()
+    # print(list(cur.description))
+    # fields = list(map(lambda x:list(x)[0], list(cur.description)))
+    # print(fields)
+    # df = pd.DataFrame(res, columns=fields)
+    # df.to_excel("wxArr.xls")
+    # file = open('wxArr.xls', 'rb')
+    # response = FileResponse(file)
+    # response['Content-Type'] = 'application/octet-stream'
+    # response['Content-Disposition'] = 'attachment;filename="wxArr.xls"'
+    return HttpResponse("")
