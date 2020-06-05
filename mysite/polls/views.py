3cdaaaa88b8fe0f2e1653810407e3b432a30dc4f
gaowenbo <gaowenbo@mankebao.com>
2020-06-05 17:20:14 +0800

    s

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,7 +25,7 @@ def wxOpen(request):
                     "code":  request.GET.get('code', "0")
                 }
     res = requests.get(url="https://api.weixin.qq.com/sns/jscode2session?appId=wxf88cbbf220e72ba5&"
-                           "secret=60f06a66c1f425b1883dd4381d728ecb&code=" + request.GET.get('code', "0"), data=data).text
+                           "secret=60f06a66c1f425b1883dd4381d728ecb&grant_type=authorization_code&js_code=" + request.GET.get('code', "0"), data=data).text
     # db = pymysql.connect(host="47.95.49.67",
     #                      port=8066,
     #                      user="sidb",
